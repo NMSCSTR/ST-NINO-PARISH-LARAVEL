1d756d3c13a8fd7432d1406629c5ce679dc0dc5d
NMSCSTR <rhondel.pagobo@nmsc.edu.ph>
2025-11-25 09:59:47 +0800

    haaala yawaa

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,7 +14,7 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware): void {
         //
         $middleware->alias([
-            'user_role' => UserRole::class,
+            'user_role' => userRole::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
