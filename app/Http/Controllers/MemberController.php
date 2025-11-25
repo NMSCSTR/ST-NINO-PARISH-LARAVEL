1d08633f7d703eecd8c27f63a2fb9a354092155f
NMSCSTR <rhondel.pagobo@nmsc.edu.ph>
2025-11-25 10:28:33 +0800

    pak

--- app/Http/Controllers/MemberController.php
+++ app/Http/Controllers/MemberController.php
@@ -15,7 +15,7 @@ class MemberController extends Controller
     public function index()
     {
         $members = Member::with('user')->get();
-        return view('admin.dashboard', compact('members','user'));
+        return view('admin.dashboard', compact('members'));
         // return $members;
     }
 
