a6031da9055010aa7cb2b6595be565b5d1af1023
NMSCSTR <rhondel.pagobo@nmsc.edu.ph>
2025-11-26 00:54:45 +0800

    Seeders update cvbnjdsf

--- database/seeders/MembersSeeder.php
+++ database/seeders/MembersSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 use App\Models\Member;
 use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class MembersSeeder extends Seeder
 {
