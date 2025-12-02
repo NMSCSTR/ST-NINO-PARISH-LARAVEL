5e058e4c32fda75c689f61a10e884ae6dcd33a04
NMSCSTR <rhondel.pagobo@nmsc.edu.ph>
2025-12-02 14:38:33 +0800

    rewrite route

--- routes/web.php
+++ routes/web.php
@@ -1,4 +1,5 @@
 <?php
+
 use App\Http\Controllers\AuthUserController;
 use App\Http\Controllers\EventController;
 use App\Http\Controllers\MemberController;
@@ -9,71 +10,83 @@ use App\Http\Controllers\SacramentController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
+/*
+|--------------------------------------------------------------------------
+| Public Routes
+|--------------------------------------------------------------------------
+*/
+
 Route::get('/', [AuthUserController::class, 'index'])->name('welcome');
 Route::get('/events/data', [EventController::class, 'fetchEvents'])->name('events.welcome');
 
-Route::get('/login', function () {
-    return view('login');
-})->name('login');
-
-Route::get('/register', function () {
-    return view('register');
-})->name('register');
+Route::get('/login', fn() => view('login'))->name('login');
+Route::get('/register', fn() => view('register'))->name('register');
 
 Route::post('/register', [UserController::class, 'store'])->name('user.store');
 Route::post('/login', [AuthUserController::class, 'auth'])->name('user.login');
 Route::post('/logout', [AuthUserController::class, 'destroy'])->name('logout');
 
+
+/*
+|--------------------------------------------------------------------------
+| Authenticated Routes
+|--------------------------------------------------------------------------
+*/
 Route::middleware('auth')->group(function () {
 
-    Route::prefix('admin')->middleware('user_role:staff,admin')->group(function () {
-        Route::get('/dashboard', [RouteController::class, 'admin'])->name('admin.dashboard');
+    /*
+    |--------------------------------------------------------------------------
+    | Admin Routes
+    |--------------------------------------------------------------------------
+    */
+    Route::prefix('admin')->middleware('user_role:staff,admin')->name('admin.')->group(function () {
 
-        Route::get('/users', [UserController::class, 'index'])->name('admin.users');
-        Route::post('/users', [UserController::class, 'addUser'])->name('users.addUsers');
+        Route::get('/dashboard', [RouteController::class, 'admin'])->name('dashboard');
+
+        // Users
+        Route::get('/users', [UserController::class, 'index'])->name('users');
+        Route::post('/users', [UserController::class, 'addUser'])->name('users.add');
         Route::get('/users/{id}/edit', [UserController::class, 'edit'])->name('users.edit');
         Route::put('/users/{id}', [UserController::class, 'update'])->name('users.update');
         Route::delete('/users/{id}', [UserController::class, 'destroy'])->name('users.destroy');
 
-        Route::get('/reservations', [ReservationController::class, 'index'])->name('admin.reservations');
+        // Reservations
+        Route::get('/reservations', [ReservationController::class, 'index'])->name('reservations');
         Route::post('/reservations/{id}/approve', [ReservationController::class, 'approve'])->name('reservations.approve');
         Route::get('/reservations/{id}/edit', [ReservationController::class, 'edit'])->name('reservations.edit');
         Route::put('/reservations/{id}', [ReservationController::class, 'update'])->name('reservations.update');
         Route::delete('/reservations/{id}', [ReservationController::class, 'destroy'])->name('reservations.destroy');
 
-        Route::get('/members', [MemberController::class, 'index'])->name('admin.members');
-        Route::get('/events', [EventController::class, 'index'])->name('admin.events');
-        Route::get('/payments', [PaymentController::class, 'index'])->name('admin.payments');
-        Route::get('/documents', [RouteController::class, 'documents'])->name('admin.documents');
+        // Members, Events, Payments, Documents
+        Route::get('/members', [MemberController::class, 'index'])->name('members');
+        Route::get('/events', [EventController::class, 'index'])->name('events');
+        Route::get('/payments', [PaymentController::class, 'index'])->name('payments');
+        Route::get('/documents', [RouteController::class, 'documents'])->name('documents');
 
-
-        Route::get('/sacraments', [SacramentController::class, 'index'])->name('admin.sacraments.index');
-        Route::get('/sacraments/create', [SacramentController::class, 'create'])->name('admin.sacraments.create');
-        Route::post('/sacraments', [SacramentController::class, 'store'])->name('admin.sacraments.store');
-        Route::get('/sacraments/{sacrament}', [SacramentController::class, 'show'])->name('admin.sacraments.show');
-        Route::get('/sacraments/{sacrament}/edit', [SacramentController::class, 'edit'])->name('admin.sacraments.edit');
-        Route::put('/sacraments/{sacrament}', [SacramentController::class, 'update'])->name('admin.sacraments.update');
-        Route::delete('/sacraments/{sacrament}', [SacramentController::class, 'destroy'])->name('admin.sacraments.destroy');
-
-
+        // Sacraments
+        Route::prefix('sacraments')->name('sacraments.')->group(function () {
+            Route::get('/', [SacramentController::class, 'index'])->name('index');
+            Route::get('/create', [SacramentController::class, 'create'])->name('create');
+            Route::post('/', [SacramentController::class, 'store'])->name('store');
+            Route::get('/{sacrament}', [SacramentController::class, 'show'])->name('show');
+            Route::get('/{sacrament}/edit', [SacramentController::class, 'edit'])->name('edit');
+            Route::put('/{sacrament}', [SacramentController::class, 'update'])->name('update');
+            Route::delete('/{sacrament}', [SacramentController::class, 'destroy'])->name('destroy');
+        });
     });
 
-    Route::prefix('member')->middleware('user_role:member')->group(function () {
-        Route::get('/dashboard', [MemberController::class, 'index'])->name('member.dashboard');
-        Route::get('/reservation', [MemberController::class, 'reservation'])->name('member.reservation');
-        Route::post('/reservation', [ReservationController::class, 'makeReservation'])->name('member.makeReservation');
+    /*
+    |--------------------------------------------------------------------------
+    | Member Routes
+    |--------------------------------------------------------------------------
+    */
+    Route::prefix('member')->middleware('user_role:member')->name('member.')->group(function () {
 
-        // Route::get('/events', [EventController::class, 'fecthEvents'])->name('member.events');
-        Route::get('/events', function () {
-            return view('member.events');
-        })->name('events.page');
+        Route::get('/dashboard', [MemberController::class, 'index'])->name('dashboard');
+        Route::get('/reservation', [MemberController::class, 'reservation'])->name('reservation');
+        Route::post('/reservation', [ReservationController::class, 'makeReservation'])->name('makeReservation');
+
+        Route::get('/events', fn() => view('member.events'))->name('events.page');
         Route::get('/events/data', [EventController::class, 'fetchEventsData'])->name('events.data');
-
     });
 });
-
-// Laravel Task Scheduler
-// Accessors And Mutators
-// Tinker
-// Event and Listeners
-//Facade
