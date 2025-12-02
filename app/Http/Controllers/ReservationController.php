2f854f90f2ca864a4ec0053dd9661613aa16c82c
NMSCSTR <rhondel.pagobo@nmsc.edu.ph>
2025-12-02 23:01:31 +0800

    payment

--- app/Http/Controllers/ReservationController.php
+++ app/Http/Controllers/ReservationController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 use App\Models\Member;
 use App\Models\Reservation;
 use App\Models\Sacrament;
+use App\Models\Payment;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
