f0b1ba44b56671e29f7ac14cf35e8d6df774b238
NMSCSTR <rhondel.pagobo@nmsc.edu.ph>
2025-12-15 06:46:21 +0800

    try updated sms for priest add HTTP

--- app/Http/Controllers/ReservationController.php
+++ app/Http/Controllers/ReservationController.php
@@ -7,6 +7,7 @@ use App\Models\Reservation;
 use App\Models\ReservationDocument;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 
 class ReservationController extends Controller
 {
