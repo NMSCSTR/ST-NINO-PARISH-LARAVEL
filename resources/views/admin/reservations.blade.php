7df5a8137f8e18b3c78c7fa04864b64cf17f262c
NMSCSTR <rhondel.pagobo@nmsc.edu.ph>
2025-12-17 01:05:42 +0800

    enhance edt

--- resources/views/admin/reservations.blade.php
+++ resources/views/admin/reservations.blade.php
@@ -168,7 +168,7 @@
                                             </button>
 
                                             <!-- Dropdown menu -->
-                                            <div class="origin-bottom-right absolute right-0 bottom-full mb-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 hidden z-50"
+                                            <div class="origin-bottom-right absolute right-0 bottom-full mb-2 w-56 max-h-96 overflow-auto rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 hidden z-50"
                                                 id="dropdown-{{ $reservation->id }}">
                                                 <div class="py-1" role="menu" aria-orientation="vertical"
                                                     aria-labelledby="menu-button-{{ $reservation->id }}">
